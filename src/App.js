bd2e61b022f30f39b7d587f73ed34a86e66741f7
Agnieszka Polińska <agapoli@gmail.com>
2021-06-04 12:20:54 +0200

    Install react-router-dom and prepare basic app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Home = () => <h1>Strona startowa</h1>
+const News = () => <h1>Aktualności</h1>
+const Contact = () => <h1>Kontakt do nas</h1>
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <Router>
+        <>
+          <header>
+            <nav>
+              <ul>
+                <li><Link to="/">Start</Link></li>
+                <li><Link to="/news">Aktualności</Link></li>
+                <li><Link to="/contact">Kontakt</Link></li>
+              </ul>
+            </nav>
+          </header>
+          <section>
+            <Route path="/" exact component={Home}/>
+            <Route path="/contact" component={Contact}/>
+            <Route path="/news" component={News}/>
+          </section>
+        </>
+      </Router>
+    );
+  }
 }
-
+ 
 export default App;
+
