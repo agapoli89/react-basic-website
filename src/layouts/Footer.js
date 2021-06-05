bebebdf634a25078d5af2206d11070df509038e5
Agnieszka Polińska <agapoli@gmail.com>
2021-06-05 18:51:57 +0200

    Implement Footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,6 +1,17 @@
+import { Route } from 'react-router-dom';
+import '../styles/Footer.css';
+
 const Footer = () => {
     return ( 
-        <div>Footer</div>
+        <div>
+            <h2>Moja strona 2021</h2>
+            <Route path="/" exact render={props => <p>Jesteś na stronie głównej</p>} />
+            <Route path="/:page" exact render={props => {
+            return (
+                <p>Jesteś na <span>{props.match.params.page}</span></p>
+            )
+            }} />
+        </div>
     );
 }
  
