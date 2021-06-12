a2f0675f7d95a62500506a45ab79c303f0124106
Agnieszka Polińska <agapoli@gmail.com>
2021-06-12 16:06:10 +0200

    Remove unusefull comments

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -4,23 +4,14 @@ import { useContext, /* useEffect, useHistory */ } from 'react';
 import { PermissionContext } from './PermissionContext';
 
 const LoginPage = () => {
-    const { /* isUserLogged, */ toggleLoggedState } = useContext(PermissionContext);
-    //const history = useHistory();
-
-    /* useEffect(() => {
-        if (isUserLogged) {
-            // eslint-disable-next-line no-restricted-globals
-          history.push("/admin");
-        }
-        // eslint-disable-next-line no-restricted-globals
-    },[isUserLogged,history]) */
+    const { toggleLoggedState } = useContext(PermissionContext);
 
     return ( 
-        <form className="login__form">
+        <div className="login__form">
             <label htmlFor="login">Podaj login<input type="text" id="login" className="login__input"/></label>
             <label htmlFor="password">Podaj hasło<input type="password" id="password" className="login__input"/></label>
             <button className="login__button" onClick={toggleLoggedState}>Zaloguj</button>
-        </form>
+        </div>
     );
 }
  
