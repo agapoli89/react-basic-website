3992390e42445b9eef68fb4135df05ac65289857
Agnieszka Polińska <agapoli@gmail.com>
2021-06-12 16:07:18 +0200

    Use useHistory to go to admin page after login

--- src/pages/PermissionContext.js
+++ src/pages/PermissionContext.js
@@ -1,12 +1,25 @@
 import { createContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export const PermissionContext = createContext();
 
 const AppProvider = ({children}) => {
 
+    const history = useHistory();
     const [isUserLogged, setIsUserLogged] = useState(true);
 
-    const handleToggleStateIsLogged = () => setIsUserLogged(prevValue => !prevValue);
+    const handleToggleStateIsLogged = () => {
+        setIsUserLogged(prevValue => !prevValue);
+
+        const location = {
+            pathname: '/admin',
+            state: {
+                isActive: true,
+            }
+        };
+
+        history.push(location);
+    };
 
     return ( 
         <PermissionContext.Provider 
